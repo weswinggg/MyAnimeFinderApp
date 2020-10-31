b91c7211777e27f69999d06f90e37e7bcc81ef10
weswinggg <weslee81412@gmail.com>
2020-10-31 18:07:16 +0800

    Initial Commit

---
+++ AnimeFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimeFinder
+//
+//  Created by Al Wesley Salac on 10/31/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
